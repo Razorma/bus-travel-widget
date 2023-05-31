1517d69cf9b441a262f351a9e37a1eb622e79b63
bheka <bheka.bj@gmail.com>
2023-05-31 16:02:59 +0200

    added responsiveness for large screens

--- DOM/bus-travel-widget-dom.js
+++ DOM/bus-travel-widget-dom.js
@@ -7,22 +7,47 @@ const singleTrips = document.querySelector("#singleTrips")
 const pricePerTrip = document.querySelector("#pricePerTrip")
 const returnTrips = document.querySelector("#returnTrips")
 const returnTripsPrice = document.querySelector("#returnTripsPrice")
+const errorMessage = document.querySelector(".errorMessage")
 //get my function 
 let tripsFunction = calculateTrips()
 
 //add an eevengt listener to my button
-calculateButton.addEventListener("click",function(){
-    if(pointsInput.value===""){
-        console.log('lolo')
-    }else if(selectLocation.value===""){
-        
-    }else{
+calculateButton.addEventListener("click", function () {
+    if (pointsInput.value === "") {
+        errorMessage.innerHTML = "please enter points"
+        setTimeout(function () {
+            errorMessage.innerHTML = '';
+        }, 3500);
+    } else if (selectLocation.value === "") {
+        errorMessage.innerHTML = "please select location"
+        setTimeout(function () {
+            errorMessage.innerHTML = '';
+        }, 3500);
+
+    } else {
         const checkedPeakButton = document.querySelector('input[name="peakTime"]:checked');
+        const returnButton = document.querySelector('input[name="return"]:checked');
         if (checkedPeakButton) {
-            tripsFunction.setpricePerTrip(selectLocation.value,checkedPeakButton.value)
-        }else{
-            console.log("enter button")
+            tripsFunction.setpricePerTrip(selectLocation.value, checkedPeakButton.value)
+            tripsFunction.setSingleTrips(130)
+            singleTrips.innerHTML = tripsFunction.getSingleTrips()
+            pricePerTrip.innerHTML = tripsFunction.getpricePerTrip()
+
+            if (returnButton) {
+                tripsFunction.setReturnTripsPrice()
+                tripsFunction.setReturnTrips()
+                returnTrips.innerHTML = tripsFunction.getReturnTrips()
+                returnTripsPrice.innerHTML = tripsFunction.getReturnTripsPrice()
+            } else {
+                returnTrips.innerHTML = ""
+                returnTripsPrice.innerHTML = ""
+            }
+        } else {
+            errorMessage.innerHTML = "please enter time 'peak or off peak'"
+            setTimeout(function () {
+                errorMessage.innerHTML = '';
+            }, 3500);
         }
-        console.log(checkedPeakButton.value)
+
     }
 })
--- bus-travel-widget.js
+++ bus-travel-widget.js
@@ -1,62 +1,62 @@
-function calculateTrips(){
+function calculateTrips() {
     //initialise a vaiable to zero where you will store your price
     let trip = 0;
     //initialize a variable to zero where you are going to store your single trips
-    let singleTrips = 0; 
+    let singleTrips = 0;
     //initialize a variable to zero where you are going to store your return trips
-    let returnTrips = 0; 
+    let returnTrips = 0;
     //initialize a variable to zero where you are going to store your return trips
-    let returnTripsPrice = 0; 
+    let returnTripsPrice = 0;
     //create a function that will take location an time as parameters and return the price depending if it is peak time or nor
-    function setpricePerTrip(location,time){
+    function setpricePerTrip(location, time) {
         //create a condition that will check the location and time
-        if(location==="Khayelitsha"){
-           if(time==="peak"){
-            trip = 50 
-          }else{
-            trip = 40
-          }
-        }else if(location==="Dunoon"){
-           if(time==="peak"){
-              trip = 31.25 
-            }else{
-              trip = 25
+        if (location === "Khayelitsha") {
+            if (time === "peak") {
+                trip = 50
+            } else {
+                trip = 40
             }
-        }else if(location==="MitchellsPlain"){
-           if(time==="peak"){
-              trip = 37.5 
-            }else{
-              trip = 30
+        } else if (location === "Dunoon") {
+            if (time === "peak") {
+                trip = 31.25
+            } else {
+                trip = 25
             }
-        }   
+        } else if (location === "MitchellsPlain") {
+            if (time === "peak") {
+                trip = 37.5
+            } else {
+                trip = 30
+            }
+        }
     }
     //create a function that will calculate how many trips one will take using their points
     //the paramete should be the points
-    function setSingleTrips(point){
-        singleTrips = (point/trip)
+    function setSingleTrips(point) {
+        singleTrips = (point / trip)
     }
     //create a function that will calculate the return trip using the trip price
-    function setReturnTripsPrice(){
-        returnTripsPrice = (trip*2)
+    function setReturnTripsPrice() {
+        returnTripsPrice = (trip * 2)
     }
     //create a fuction that will get the number of return trips posible
-    function setReturnTrips(){
-        returnTrips = parseInt((parseInt(singleTrips)/2))
+    function setReturnTrips() {
+        returnTrips = parseInt((parseInt(singleTrips) / 2))
     }
     //create a function that will return the trip cost
-    function getpricePerTrip(){
+    function getpricePerTrip() {
         return trip
     }
     //create a function that will return the number of trips possible
-    function getSingleTrips(){
+    function getSingleTrips() {
         return parseInt(singleTrips)
     }
     //create a functio that will return the number of return trips posiple
-    function getReturnTrips(){
+    function getReturnTrips() {
         return returnTrips
     }
     //create a function that will return the cost of a return trip
-    function getReturnTripsPrice(){
+    function getReturnTripsPrice() {
         return returnTripsPrice
     }
     return {
